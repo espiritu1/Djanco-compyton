e42314b0bfba59b0391c4d8b7c852553e855543c
fere <tiran.sc.f@gmail.com>
2022-04-10 18:33:40 -0500

    intentnado subir a heroku

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-u%r)2ngi#-74psd
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
